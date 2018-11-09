69e33aa26c51e594abc4fc3d6d9e6ddc3ff49e03
allsportscoin <kirkcong@gmail.com>
2018-11-09 09:38:43 +0000

    add sns contract

--- contracts/demo/sns.sol
+++ contracts/demo/sns.sol
@@ -1,150 +1,170 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.16;
 
- contract owned {
-     address public owner;
+contract owned {
+    address public owner;
+    constructor() public {
+        owner = msg.sender;
+    }
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+}
 
-     function owned() public {
-         owner = msg.sender;
-     }
+contract restriction {
+    modifier lengthLimit(string name) {
+        bytes memory strBytes = bytes(name);
+        require(strBytes.length>6, "domain length too short");
+        _;
+    }
 
-     modifier onlyOwner {
-         require(msg.sender == owner);
-         _;
-     }
+    modifier validChar(string name) {
 
- }
+        _;
+    }
+}
 
- contract stringLib {
+contract stringLib {
 
-     function bytes32ToString(bytes32 x) internal constant returns (string) {
-         bytes memory bytesString = new bytes(32);
-         uint charCount = 0;
-         for (uint j = 0; j < 32; j++) {
-             byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-             if (char != 0) {
-                 bytesString[charCount] = char;
-                 charCount++;
-             }
-         }
-         bytes memory bytesStringTrimmed = new bytes(charCount);
-         for (j = 0; j < charCount; j++) {
-             bytesStringTrimmed[j] = bytesString[j];
-         }
-         return string(bytesStringTrimmed);
-     }
+    function bytes32ToString(bytes32 x) internal pure returns (string) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
+    }
 
-     function bytes32ArrayToString(bytes32[] data) internal constant returns (string) {
-         bytes memory bytesString = new bytes(data.length * 32);
-         uint urlLength;
-         for (uint i = 0; i< data.length; i++) {
-             for (uint j = 0; j < 32; j++) {
-                 byte char = byte(bytes32(uint(data[i]) * 2 ** (8 * j)));
-                 if (char != 0) {
-                     bytesString[urlLength] = char;
-                     urlLength += 1;
-                 }
-             }
-         }
-         bytes memory bytesStringTrimmed = new bytes(urlLength);
-         for (i = 0; i < urlLength; i++) {
-             bytesStringTrimmed[i] = bytesString[i];
-         }
-         return string(bytesStringTrimmed);
-     }
+    function bytes32ArrayToString(bytes32[] data) internal pure returns (string) {
+        bytes memory bytesString = new bytes(data.length * 32);
+        uint urlLength;
+        for (uint i = 0; i< data.length; i++) {
+            for (uint j = 0; j < 32; j++) {
+                byte char = byte(bytes32(uint(data[i]) * 2 ** (8 * j)));
+                if (char != 0) {
+                    bytesString[urlLength] = char;
+                    urlLength += 1;
+                }
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(urlLength);
+        for (i = 0; i < urlLength; i++) {
+            bytesStringTrimmed[i] = bytesString[i];
+        }
+        return string(bytesStringTrimmed);
+    }
+}
 
- }
+contract SNS is owned, restriction, stringLib {
+    bytes4[] public RESOLVER_INTERFACE_ID = [bytes4(0x3912718c),bytes4(0x351af6a0),bytes4(0x51119ae1),bytes4(0xc67b08d2)];
+    bytes4 SNS_ID = 0xef45283a;
 
- contract SNS is owned, stringLib{
+    struct Suffix {
+        string suffix;
+        address addr;
+        bool isAvaliable;
+        string content;
+        // uint256 fee;
+    }
 
-     struct Record {
-         address addr;
-         string content;
-         bytes32 bname;
-         string name;
-         string suffix;
-         bool isValue;
+    mapping(string=>Suffix) internal Suffixes;
 
-     }
+    string[] Names;
 
-     mapping(bytes32 => Record) public Records;
-     bytes32[] internal Names;
+    mapping(address=>string) internal AddressToSuffix;
 
-     mapping(bytes32 => uint) internal suffix_level; // 1: onlyOwner, >1 userable
-     string[] public Suffixs;
+    event AddSuffix(string _suffix, address _address);
 
-     mapping(address => bytes32) public Address2Name;
-     uint256 public fee = 1;
+    constructor() public {
 
-     event Transfer(address _from, address _to, string _name);
-     event ChangeName(address _owner, string _old, string _new);
-     event NewSuffix(string _suffix, uint owner);
+    }
 
-     function SNS(uint256 _fee) payable {
-         if(_fee > 0){
-             fee = _fee;
-         }
-     }
+    function addSuffix(string _suffixName, address _addr) public onlyOwner validChar(_suffixName) {
+        require(!Suffixes[_suffixName].isAvaliable, "suffix already used");
+        // require(AddressToSuffix[_addr]>0,"");
+        require(bytes(AddressToSuffix[_addr]).length==0, "address already bind to other suffix resolver");
 
-     function changeFee(uint256 _fee) public onlyOwner {
-         fee = _fee;
-     }
+        require(supportSNSInterface(_addr), "resolver contract not support sns standard");
+        Names.push(_suffixName);
+        Suffixes[_suffixName] = Suffix(_suffixName, _addr, true, "");
+        AddressToSuffix[_addr] = _suffixName;
+        emit AddSuffix(_suffixName, _addr);
+    }
 
-     //for owner add some suffix for users, just like, "soc","user" etc
-     function addSuffix(bytes32 _suffix, uint _ownerOnly) public onlyOwner {
-         suffix_level[_suffix] = _ownerOnly;
-         string memory suffix = bytes32ToString(_suffix);
-         Suffixs.push(suffix);
-         NewSuffix(suffix, _ownerOnly);
-     }
+    function getSuffixByName(string _suffixName) public constant returns(address _addr,
+        bool _isAvaliable,
+        string _content) {
+        require(Suffixes[_suffixName].isAvaliable, "suffix not exist");
+        Suffix memory _suffix = Suffixes[_suffixName];
+        _addr = _suffix.addr;
+        _isAvaliable = _suffix.isAvaliable;
+        _content = _suffix.content;
+    }
 
-     function getRecord(bytes32 _name) public returns(address _addr, string _suffix) {
-         require(Records[_name].isValue, 'this name not used');
-         Record record = Records[_name];
-         _addr = record.addr;
-         _suffix = record.suffix;
-     }
+    function getSuffixByAddr(address _addr) public constant returns(string _suffixName,
+        bool _isAvaliable,
+        string _content) {
+        require(bytes(AddressToSuffix[_addr]).length>0, "address not bind to any suffix");
+        _suffixName = AddressToSuffix[_addr];
+        Suffix memory _suffix = Suffixes[_suffixName];
+        _isAvaliable = _suffix.isAvaliable;
+        _content = _suffix.content;
+    }
 
-     //user can bind a name and suffix with his address, when the suffix exists and the name is unused
-     //Anyone who want bind name should pay some token
-     //only name owner can change his domain name
-     function setName(bytes32 _name, bytes32 _suffix) payable public {
-         require(msg.value >= fee, 'must pay some value from bind name');
-         require(suffix_level[_suffix] > 0, 'no such suffix');
-         require(!Records[_name].isValue, 'this name allready used');
-         if(suffix_level[_suffix] == 1){
-             require(msg.sender == owner, 'this suffix only owner used');
-         }
+    function setAliasOfSuffix(string _suffixName, string _aliasName, address _aliasAddr) public payable {
+        require(Suffixes[_suffixName].isAvaliable, "suffix not exist");
+        Suffix memory _suffix = Suffixes[_suffixName];
+        Resolver _resol = Resolver(_suffix.addr);
+        uint256 fee = msg.value;
+        _resol.setAlias.value(fee)(_aliasName, _aliasAddr);
+    }
 
-         string memory name = bytes32ToString(_name);
-         string memory suffix = bytes32ToString(_suffix);
-         Record memory record = Record(msg.sender,'',_name,name,suffix,true);
+    function getAliasByAddrAndSuffix(string _suffixName, address _aliasAddr) public constant returns(string) {
+        require(Suffixes[_suffixName].isAvaliable, "suffix not exist");
+        Suffix memory _suffix = Suffixes[_suffixName];
+        Resolver _resol = Resolver(_suffix.addr);
+        return _resol.getAliasByAddr(_aliasAddr);
+    }
 
-         Names.push(_name);
-         Records[_name] = record;
+    function getAddrByAliasAndSuffix(string _suffixName, string _aliasName) public constant returns(address) {
+        require(Suffixes[_suffixName].isAvaliable, "suffix not exist");
+        Suffix memory _suffix = Suffixes[_suffixName];
+        Resolver _resol = Resolver(_suffix.addr);
+        return _resol.getAddrByAlias(_aliasName);
+    }
 
-         string memory oldName = 'no name';
-         if (Address2Name[msg.sender].length > 0){
-             bytes32 oldbname = Address2Name[msg.sender];
-             oldName = bytes32ToString(oldbname);
-             delete Records[oldbname];
-         }
-         Address2Name[msg.sender] = _name;
-         ChangeName(msg.sender, oldName, name);
+    function supportSNSInterface(address _addr) public view returns(bool) {
+        Resolver _resol = Resolver(_addr);
+        return _resol.supportSNSInterface(RESOLVER_INTERFACE_ID[0]) &&
+            _resol.supportSNSInterface(RESOLVER_INTERFACE_ID[1]) &&
+            _resol.supportSNSInterface(RESOLVER_INTERFACE_ID[2]) &&
+            _resol.supportSNSInterface(RESOLVER_INTERFACE_ID[3]);
+    }
 
-     }
+    function withDraw(address addr) public onlyOwner {
+        require(addr != 0x0);
+        addr.transfer(address(this).balance);
+    }
+}
 
-     //user can transfer his name to other
-     function transferName(bytes32 _name, address _addr) public {
-         require(Records[_name].isValue);
-         Record record = Records[_name];
-         require(record.addr == msg.sender, 'no Permission Change the record');
+interface Resolver {
 
-         record.addr = _addr;
-         Transfer(msg.sender, _addr, bytes32ToString(_name));
-     }
+    function setAlias(string _aliasName, address _aliasAddr) public payable;
 
-     //with draw
-     function withDraw() onlyOwner {
-         owner.transfer(address(this).balance);
-     }
- }
+    function getAddrByAlias(string _aliasName) public view returns(address);
+
+    function getAliasByAddr(address _addr) public view returns(string);
+
+    function supportSNSInterface(bytes4 _interfaceId) public view returns(bool);
+
+}
+
+
